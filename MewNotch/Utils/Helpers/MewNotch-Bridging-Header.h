a556852a4b3f33d065ee13c146ac952335b736d2
Monu Kumar <monuk7735@gmail.com>
2025-03-09 22:04:28 +0530

    mod: ui: hud: progress

--- MewNotch/Utils/Helpers/MewNotch-Bridging-Header.h
+++ MewNotch/Utils/Helpers/MewNotch-Bridging-Header.h
@@ -10,5 +10,4 @@
 #import <Foundation/Foundation.h>
 
 #import "Brightness/Brightness.h"
-
 #import "AudioControl/AudioControl.h"
--- MewNotch/View/Notch/Collapsed/HUDView/ProgressHUDView.swift
+++ MewNotch/View/Notch/Collapsed/HUDView/ProgressHUDView.swift
@@ -7,63 +7,116 @@
 
 import SwiftUI
 
+import Lottie
+
 struct ProgressHUDView: View {
     
     @ObservedObject var notchViewModel: CollapsedNotchViewModel
     
     var body: some View {
-        if let hudValue = notchViewModel.hudValue {
-            RoundedRectangle(
-                cornerRadius: 99
-            )
-            .fill(
-                .white.opacity(
-                    0.1
-                )
-            )
-            .frame(
-                height: 8
-            )
-            .overlay {
-                GeometryReader { geometry in
-                    RoundedRectangle(
-                        cornerRadius: 99
+        if let hudLottie = notchViewModel.hudIcon, let hudValue = notchViewModel.hudValue {
+            VStack {
+                HStack {
+                    Text(
+                        "000 %"
                     )
-                    .fill(Color.accentColor)
-                    .frame(
-                        width: CGFloat(hudValue) * geometry.size.width,
-                        height: geometry.size.height
+                    .font(.title3.bold())
+                    .opacity(0)
+                    .overlay {
+                        LottieView(
+                            animation: hudLottie
+                        )
+                        .currentProgress(Double(hudValue))
+                        .configuration(
+                            .init(
+                                renderingEngine: .mainThread
+                            )
+                        )
+                        .colorInvert()
+                        .scaledToFit()
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    }
+                    
+                    Spacer()
+                    
+                    Text(
+                        "000 %"
                     )
-                    .frame(
-                        width: geometry.size.width,
-                        alignment: .leading
-                    )
+                    .font(.title3.bold())
+                    .opacity(0)
+                    .overlay {
+                        AnimatedTextView(
+                            value: Double(hudValue * 100)
+                        ) { value in
+                            Text(
+                                String(
+                                    format: "%02.0f %",
+                                    value
+                                )
+                            )
+                            .font(.title3.bold())
+                            .foregroundStyle(Color.white)
+                        }
+                        .frame(maxWidth: .infinity, alignment: .trailing)
+                    }
                 }
-            }
-            .clipShape(
-                RoundedRectangle(
-                    cornerRadius: 99
-                )
-            )
-            .background {
+                
                 RoundedRectangle(
                     cornerRadius: 99
                 )
                 .fill(
                     .white.opacity(
-                        0.2
+                        0.1
                     )
                 )
-                .blur(
-                    radius: 3
+                .frame(
+                    height: 8
                 )
+                .overlay {
+                    GeometryReader { geometry in
+                        Rectangle()
+                            .fill(Color.accentColor)
+                            .frame(
+                                width: CGFloat(hudValue) * geometry.size.width,
+                                height: geometry.size.height
+                            )
+                            .frame(
+                                width: geometry.size.width,
+                                alignment: .leading
+                            )
+                    }
+                }
+                .clipShape(
+                    RoundedRectangle(
+                        cornerRadius: 99
+                    )
+                )
+                .background {
+                    RoundedRectangle(
+                        cornerRadius: 99
+                    )
+                    .fill(
+                        .white.opacity(
+                            0.2
+                        )
+                    )
+                    .blur(
+                        radius: 3
+                    )
+                }
             }
             .padding(
-                24
+                .horizontal, 10
+            )
+            .padding(
+                .bottom,
+                10
+            )
+            .padding(
+                .horizontal, notchViewModel.extraNotchPadSize.width / 2
             )
             .frame(
-                width: notchViewModel.notchSize.width,
-                height: notchViewModel.notchSize.height
+                width: notchViewModel.notchSize.width
             )
             .transition(
                 .move(
